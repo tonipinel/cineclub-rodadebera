694f9b7ba2de7073256149ab1473d5a5c2240b06
Toni Pinel <tonipinel@gmail.com>
2026-08-18 14:34:10 +0200

    Switch heading font to Oswald and style header brand text in gold uppercase

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
         },
       },
       fontFamily: {
-        display: ["Comfortaa", "sans-serif"],
+        display: ["Oswald", "sans-serif"],
         body: ["Inter", "sans-serif"],
       },
     },
